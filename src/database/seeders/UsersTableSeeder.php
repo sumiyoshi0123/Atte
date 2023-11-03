9f493446eb5790b9250373e8029dffa9d5d83551
sumiyoshi0123 <blackcheery_hyasude@yahoo.co.jp>
2023-11-03 21:41:50 +0900

    更新

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Auth\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'test',
+            'email' => 'abc@gmail.com',
+            'password' => Hash::make('12345678')
+        ];
+
+        User::create($param);
+
+    }
+}
---
+++ src/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login',function (Request $request) {
+    $credentials = $request -> only('email','password');
+
+    if (Auth::attempt($credentials)){
+        $user = Auth::user();
+        $token = $user -> createToken('token-name')-> plainTextToken;
+        return response()->json(['token' => $token]);
+    }
+
+    return response()->json(['error' => 'Unauthorized'],401);
+});
+
