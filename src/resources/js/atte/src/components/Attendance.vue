e453565e863f46602924d9707f18388c99913050
sumiyoshi0123 <blackcheery_hyasude@yahoo.co.jp>
2023-12-08 09:27:45 +0900

    更新

---
+++ src/resources/js/atte/src/components/Attendance.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+const attendances = ref([
+    {
+        "id": 1,
+        "user_id": 1,
+        "date": "2023-11-28",
+        "start_time": "10:00:00",
+        "end_time": "20:00:00",
+        "created_at": null,
+        "updated_at": null
+    },
+]);
+
+onMounted(async () => {
+    const json = await axios.get("http://localhost/api/attendance");
+    const data = json.data.items[0];
+    attendances.value = data;
+});
+</script>
+
+<template>
+    <table>
+        <tr>
+            <th>名前</th>
+            <th>勤務開始</th>
+            <th>勤務終了</th>
+            <th>休憩時間</th>
+            <th>勤務時間</th>
+        </tr>
+        <tr v-for="attendance in attendances" >
+            <th>{{ attendance.name }}</th>
+            <th>{{ attendance.start_time }}</th>
+            <th>{{ attendance.end_time }}</th>
+            <th>{{  }}</th>
+            <th>{{  }}</th>
+        </tr>
+    </table>
+</template>
--- src/resources/js/atte/src/main.js
+++ src/resources/js/atte/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
+
+const token = "3|OfnPEfpiWjLA46k7zgpmtXqKxqDTsfF5dumiWNRW";
+axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 createApp(App).mount('#app')
